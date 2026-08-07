0b36af1e879f8919f902615681b3a78db88e1c9f
Anna Ivanchenko <bubachka1990@gmail.com>
2026-08-08 01:12:29 +0300

    Implement update article controller

--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { celebrate } from "celebrate";
 
 // Імпорт контролерів article (розкоментувати, коли почнете писати код):
-// import { article as ctrl } from "../controllers/index.js";
-// import { getArticlesSchema, articleIdSchema, createArticleSchema, updateArticleSchema } from "../validations/index.js";
+import { article as ctrl } from "../controllers/index.js";
+import { getArticlesSchema, articleIdSchema, createArticleSchema, updateArticleSchema } from "../validations/index.js";
 
 const articleRoutes = Router();
 
@@ -17,10 +17,10 @@ const articleRoutes = Router();
 // articleRoutes.get("/:id", celebrate(articleIdSchema), ctrl.getArticleById);
 
 // Створити статтю (private)
-// articleRoutes.post("/", celebrate(createArticleSchema), ctrl.createArticle);
+articleRoutes.post("/", celebrate(createArticleSchema), ctrl.createArticle);
 
 // Редагувати статтю (private)
-// articleRoutes.patch("/:id", celebrate(updateArticleSchema), ctrl.updateArticle);
+articleRoutes.patch("/:id", celebrate(updateArticleSchema), ctrl.updateArticle);
 
 // Видалити статтю (private)
 // articleRoutes.delete("/:id", celebrate(articleIdSchema), ctrl.deleteArticle);
