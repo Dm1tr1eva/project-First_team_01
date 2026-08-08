66e38f46384855d152398020271c3426493e5705
Denis Gulko <denis.gulko.1998@gmail.com>
2026-08-08 22:20:38 +0300

    authLogin created

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,1 +1,22 @@
-export const login = async (req, res, next) => {};
+import createHttpError from "http-errors";
+import { User } from "../../models/User.js";
+import bcrypt from "bcrypt";
+import { Session } from "../../models/Session.js";
+export const login = async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    throw createHttpError(401, "Invalid credentials");
+  }
+
+  const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+  console.log(req.body.password, user.password);
+
+  if (!isValidPassword) {
+    throw createHttpError(401, "Invalid credentials");
+  }
+  await Session.deleteOne({ userId: user._id });
+
+  const newSession = await createSession(user._id);
+  setSessionCookies(res, newSession);
+  res.status(200).json({ user });
+};
