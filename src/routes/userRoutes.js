bbc8ee21f7911f049e2007dad24c0a03b8ece5ef
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-08-08 20:20:07 +0300

    Add feature

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { celebrate } from "celebrate";
 
 // Імпорт контролерів user (розкоментувати, коли почнете писати код):
-// import { user as ctrl } from "../controllers/index.js";
+ import { user as ctrl } from "../controllers/index.js";
 // import { updateUserSchema } from "../validations/index.js";
 
 const userRoutes = Router();
@@ -17,7 +17,7 @@ const userRoutes = Router();
 // userRoutes.patch("/me/avatar", ctrl.updateAvatar);
 
 // Отримати статті користувача (public)
-// userRoutes.get("/:id/articles", ctrl.getUserArticles);
+ userRoutes.get("/:id/articles", ctrl.getUserArticles);
 
 // Отримати збережені статті (private)
 // userRoutes.get("/me/saved", ctrl.getSavedArticles);
