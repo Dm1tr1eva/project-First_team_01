6404f3e523c44610eeb0036a7157f330b8d5e2b9
Dmitriy0212 <demarr68@gmail.com>
2026-08-08 13:22:12 +0300

    logout

--- src/controllers/auth/logout.js
+++ src/controllers/auth/logout.js
@@ -1,1 +1,13 @@
-export const logout = async (req, res, next) => {};
+import { Session } from "../../models/Session.js";
+
+export const logout = async (req, res, next) => {
+  const { sessionId } = req.cookies;
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
+  }
+  res.clearCookie("sessionId");
+  res.clearCookie("accessToken");
+  res.clearCookie("refreshToken");
+
+  res.status(204).send();
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { celebrate } from "celebrate";
 
 // Імпорт контролерів auth (розкоментувати, коли почнете писати код):
-// import { auth as ctrl } from "../controllers/index.js";
+//import { auth as ctrl } from "../controllers/index.js";
 // import { registerSchema, loginSchema } from "../validations/index.js";
 
 const authRoutes = Router();
@@ -14,7 +14,7 @@ const authRoutes = Router();
 // authRoutes.post("/login", celebrate(loginSchema), ctrl.login);
 
 // Логаут (private, потрібен authMiddleware)
-// authRoutes.post("/logout", ctrl.logout);
+//authRoutes.post("/logout", ctrl.logout);
 
 // Оновлення access/refresh токена (protected)
 // authRoutes.post("/refresh", ctrl.refresh);
