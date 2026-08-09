d3a8510589192dae81b0cf72ae7bad4df038b189
Dmitriy0212 <demarr68@gmail.com>
2026-08-09 11:57:54 +0300

    Fixing errors related to retrieving articles by ID.

--- src/controllers/article/getArticleById.js
+++ src/controllers/article/getArticleById.js
@@ -1,8 +1,11 @@
 import { Article } from "../../models/Article.js";
+import createHttpError from "http-errors";
 export const getArticleById = async (req, res, next) => {
   const { id } = req.params;
-
   const article = await Article.findById(id);
+  if (!article) {
+    throw createHttpError(404, "Article not found");
+  }
 
   res.status(200).json({
     article,
