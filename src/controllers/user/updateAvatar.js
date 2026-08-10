9fb4babf6f5e96aecf8d3ee23677e673009edd0e
Vikroria Fartushna <vika.beis123@gmail.com>
2026-08-10 14:23:04 +0300

    fix: виправлено шлях до saveFileToCloudinary, усунено зауваження рев'ю

--- src/controllers/user/updateAvatar.js
+++ src/controllers/user/updateAvatar.js
@@ -1,16 +1,26 @@
-
+import createHttpError from "http-errors";
+import { v2 as cloudinary } from "cloudinary";
 import { User } from "../../models/User.js";
+import { saveFileToCloudinary } from "../../utils/saveFileToCloudinary.js";
 
 export const updateAvatar = async (req, res, next) => {
   try {
     if (!req.file) {
-      return res.status(400).json({ error: "Будь ласка, виберіть фото" });
+      throw createHttpError(400, "Будь ласка, виберіть фото");
     }
 
-    const avatarUrl = req.file.path;
+    const currentUser = await User.findById(req.user._id);
+    const oldAvatarUrl = currentUser?.avatarUrl;
+
+    if (oldAvatarUrl) {
+      const publicId = oldAvatarUrl.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(`harmoniq/avatars/${publicId}`);
+    }
+
+    const avatarUrl = await saveFileToCloudinary(req.file.buffer, "avatars");
 
     const user = await User.findByIdAndUpdate(
-      req.user.id,
+      req.user._id,
       { avatarUrl: avatarUrl },
       { new: true }
     );
@@ -23,7 +33,6 @@ export const updateAvatar = async (req, res, next) => {
       },
     });
   } catch (error) {
-    console.error("Помилка завантаження аватара:", error);
     next(error);
   }
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,28 +3,35 @@ import { celebrate } from "celebrate";
 import { user as ctrl } from "../controllers/index.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import { articleIdSchema } from "../validations/index.js";
-import { uploadCloudinary } from "../middleware/uploadMiddleware.js";
+import { upload } from "../middleware/uploadMiddleware.js";
 import { updateUserSchema } from "../validations/userValidation.js";
 
 const userRoutes = Router();
 
+// Отримати інфо про користувача за id (public)
 userRoutes.get("/:id", ctrl.getUserInfo);
 
+// Оновити дані користувача (private)
 userRoutes.patch("/me", authMiddleware, celebrate(updateUserSchema), ctrl.updateUser);
 
+// Додати/змінити аватар (private)
 userRoutes.patch(
   "/me/avatar",
   authMiddleware,
-  uploadCloudinary.single("avatar"),
+  upload.single("avatar"),
   ctrl.updateAvatar
 );
 
+// Отримати статті користувача (public)
 userRoutes.get("/:id/articles", celebrate(articleIdSchema), ctrl.getUserArticles);
 
+// Отримати збережені статті (private)
 userRoutes.get("/me/saved", authMiddleware, ctrl.getSavedArticles);
 
+// Додати статтю у збережені (private)
 userRoutes.post("/me/saved/:id", authMiddleware, celebrate(articleIdSchema), ctrl.addSavedArticle);
 
+// Видалити статтю зі збережених (private)
 userRoutes.delete(
   "/me/saved/:id",
   authMiddleware,
