f54138de4a7d632b3b8cbbb0d4bfb4fb7b41fa07
Anna Ivanchenko <bubachka1990@gmail.com>
2026-08-11 18:34:28 +0300

    remove swagger-autogen, keep swagger.js as reference config only

--- src/swagger.js
+++ src/swagger.js
@@ -1,5 +1,3 @@
-import swaggerAutogen from "swagger-autogen";
-
 const doc = {
   info: {
     title: "Harmoniq API",
@@ -7,11 +5,4 @@ const doc = {
   },
   servers: [{ url: "/" }, { url: "http://localhost:3000" }],
 };
-
-const outputFile = "./swagger.json";
-const routes = ["./server.js"];
-
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
-
-swaggerAutogen({ openapi: "3.0.0" })(outputFile, routes, doc);
+export default doc;
