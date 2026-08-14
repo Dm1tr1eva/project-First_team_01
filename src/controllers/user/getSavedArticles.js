35b0863b7de51ded8f79ee8c1f1b63b777bff657
Vitalii Fedunyk <vitaliifedunyk.dev@gmail.com>
2026-08-14 20:03:54 +0200

    fix: align saved article response contracts

--- src/controllers/user/getSavedArticles.js
+++ src/controllers/user/getSavedArticles.js
@@ -1,5 +1,8 @@
 export const getSavedArticles = async (req, res) => {
-  const user = await req.user.populate("savedArticles");
+  const user = await req.user.populate({
+    path: "savedArticles",
+    populate: { path: "ownerId", select: "name avatarUrl" },
+  });
 
   res.status(200).json({
     savedArticles: user.savedArticles,
